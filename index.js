bdc206c695de5fbc0fde72e6825da1fde200c9e6
DiviyamPathak <patdiviyam@gmail.comm>
2021-09-09 16:18:56 +0530

    does not work with firefox

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-let minimumcal = document.getElementById("min-cal");
-let maxmimumcal = document.getElementById("max-cal");
- 
+const submitbtn = document.getElementById("subbtn");
+const minimumcal = document.getElementById("min-cal");
+const maxmimumcal = document.getElementById("max-cal");
+
+function valueallocator(){
+    minc = minimumcal.value;
+    maxc = maxmimumcal.value;
+    
+}
+// create api
+function api(){
+    valueallocator()
+    recipebycal = "https://api.spoonacular.com/recipes/findByNutrients?apiKey=5f6c3bcea03b4ac99bcabe53b6033899&minCarbs="+ minc + "&maxCarbs=" + maxc
+    
+}
+
+
+
+
+
+submitbtn.addEventListener("click",api)
+
