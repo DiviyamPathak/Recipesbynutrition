aa90ab3c3da880b884a4465c9017213b370ddca6
DiviyamPathak <patdiviyam@gmail.comm>
2021-09-13 00:57:11 +0530

    working with fetch and display

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const submitbtn = document.getElementById("subbtn");
 const minimumcal = document.getElementById("min-cal");
 const maxmimumcal = document.getElementById("max-cal");
+const displayout =  document.getElementsByClassName("output")
 
 function valueallocator(){
     minc = minimumcal.value;
@@ -11,12 +12,30 @@ function valueallocator(){
 function api(){
     valueallocator()
     recipebycal = "https://api.spoonacular.com/recipes/findByNutrients?apiKey=5f6c3bcea03b4ac99bcabe53b6033899&minCarbs="+ minc + "&maxCarbs=" + maxc
-    
+    console.log(recipebycal)
 }
 
+function fetchrecipes(){
+    api()
+    fetch(recipebycal)
+        .then(response => response.json())
+        
+        .then(displaydom)
+
+}
+function displaydom(_data){
+        for (x in _data){
+            
+     }
+    
+
+ }
 
 
 
 
-submitbtn.addEventListener("click",api)
+
+
+
+submitbtn.addEventListener("click",fetchrecipes)
 
